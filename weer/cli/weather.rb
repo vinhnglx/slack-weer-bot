25c3d8f151d20c3a1f47e2738ee05c9e6ec57944
Vinh Nguyen <vinh.nglx@gmail.com>
2016-02-07 00:20:49 +0700

    Implement slackbot command

--- weer/cli/weather.rb
+++ weer/cli/weather.rb
@@ -1,8 +1,33 @@
 module Weer
   module CLI
     class Weather < SlackRubyBot::Commands::Base
-      match /^How is the weather in (?<location>.*)\?$/ do |client, data, match|
-        client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
+      match /^[w|W]eather in (?<location>.*)$/ do |client, data, match|
+        city = match[:location]
+        results = ''
+
+        wetter = Wetter.new(city)
+        response = wetter.connect
+
+        client.say(channel: data.channel, text: "Looks like your city is not right! :wink: ", gif: 'wink') if response['query']['results'].nil?
+
+        unless response['query']['results'].nil?
+          forecast = wetter.forecast response
+          wind = wetter.wind response
+          atmosphere = wetter.atmosphere response
+
+          results << "The forecast of #{city.upcase!} in the next coming days\n"
+          results << wetter.parse_forecast(forecast).render
+          results << "\n"*2
+
+          results << "The wind power of #{Date.today.to_s}\n"
+          results << wetter.parse_wind(wind).render
+          results << "\n"*2
+
+          results << "The atmosphere of #{Date.today.to_s}\n"
+          results << wetter.parse_atmosphere(atmosphere).render
+
+          client.say(channel: data.channel, text: "```#{results}```", gif: 'have a nice day')
+        end
       end
     end
   end
